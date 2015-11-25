5a6d3f22dea6488cb75eaa957dfe2cb9c4323199
Kittiphong Xayasane <kxayasan@gmail.com>
2015-11-25 18:12:43 -0500

    Initial Commit

---
+++ MetovaSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MetovaSearch
+//
+//  Created by kittiphong xayasane on 2015-11-25.
+//  Copyright © 2015 Kittiphong Xayasane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MetovaSearch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MetovaSearch
+//
+//  Created by kittiphong xayasane on 2015-11-25.
+//  Copyright © 2015 Kittiphong Xayasane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
